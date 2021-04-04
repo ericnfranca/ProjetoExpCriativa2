fd681da90a9d8dfd852b4c6d22a9b71ae6f247f7
Ivan Garbi <ivan_garbi@hotmail.com>
2021-04-04 16:38:30 -0300

    Mandando código php

--- Public/php/config.php
+++ Public/php/config.php
@@ -12,15 +12,41 @@ $codigo_seguranca = $_POST["codigo_seguranca"];
 $nome_titular = $_POST["nome_titular"];
 $cpf = $_POST["cpf"];
 
-
-
 mysqli_query($conexao, "INSERT INTO pessoa(nome_completo, data_nascimento, email, validacao_email, senha_hash_sha256, numero_cartao, validade_cartao, codigo_seguranca, nome_titular, cpf) VALUES 
                 ('$nome_completo', '$data_nascimento', '$email', 'n', '$senha', '$numero_cartao', '$validade_cartao', '$codigo_seguranca', '$nome_titular', '$cpf')");
 
 
 
+// mysqli_close($conexao);
+
+
+
+date_default_timezone_set('Etc/UTC');
+
+
+require '../Public/php/PHPMailer/PHPMailerAutoload.php';
+
+$tituloEmail = "Confirmação de Cadastro";
+
+$message = 'E-mail cadastrado com sucesso!';
+
+$mail= new PHPMailer();
+$mail->IsSMTP(); 
+$mail->CharSet = 'UTF-8';   
+$mail->SMTPDebug = 2;       // 0 = nao mostra o debug, 2 = mostra o debug
+$mail->SMTPAuth = true;     
+$mail->SMTPSecure = 'ssl';  
+$mail->Host = 'smtp.gmail.com'; 
+$mail->Port = 465; 
+$mail->Username = 'nextfilmes.exp@gmail.com'; 
+$mail->Password = 'nextfilmes123456';
+$mail->SetFrom('nextfilmes.exp@gmail.com', 'NextFilmes');
+$mail->addAddress('josian.brais@forloans.org','');
+$mail->Subject = $tituloEmail;
+$mail->msgHTML($message);
+$mail->send();
+
+// Configure o Gmail para permitir aplicativos de terceiro 
+// https://myaccount.google.com/lesssecureapps
 mysqli_close($conexao);
-
-
-
 ?>
