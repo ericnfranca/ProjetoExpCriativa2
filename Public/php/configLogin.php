211c54f8ff7f4804ef7cdb1fea254ae5a3360979
ERICNIELSENFRANCA <you@example.com>
2021-04-07 18:32:54 -0300

    Ajustando validacao de login

--- Public/php/configLogin.php
+++ Public/php/configLogin.php
@@ -4,10 +4,11 @@ $conexao = mysqli_connect("localhost", "root", "", "projetoexpcriativa2");
 $email = $_POST["email"];
 $senha = $_POST["senha"];
 
-mysqli_query($conexao, "SELECT INTO pessoa(email, senha_hash_sha256) VALUES ('$email', '$senha')");
+//mysqli_query($conexao, "SELECT INTO pessoa(email, senha_hash_sha256) VALUES ('$email', '$senha')");
 
-$resultado = mysqli_query($conexao, "SELECT * FROM pessoa WHERE email= '$email' and senha_hash_sha256= '$senha'");
+$resultado = mysqli_query($conexao, "SELECT * FROM pessoa WHERE email= '$email' and senha_hash_sha256= '$senha' and validacao_email= 's'");
 $resultadoRow = mysqli_num_rows ($resultado);
+
 
 if ($resultadoRow == false){
     echo json_encode("n");
