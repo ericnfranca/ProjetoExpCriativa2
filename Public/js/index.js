60ebaeccc8c3014f5362f0cc66ade67b48560542
Ivan Garbi <ivan_garbi@hotmail.com>
2021-05-01 16:00:50 -0300

    modal dos filmes

--- Public/js/index.js
+++ Public/js/index.js
@@ -6,12 +6,10 @@ $(document).ready(function() {
 
 
 function fLocalComunicaServidor() {
-    
     $.ajax({
         type: 'POST',
         dataType: 'json',
         url: "../public/php/index-sessao.php",
-        
         success: function(retorno){
             if(retorno == "n"){ 
                 window.location.href = '../Views/TelaLogin.html'
@@ -41,6 +39,20 @@ function EnviarFilmesServidor() {
     })
 }
 
+function RecebeFilmesServidorModal() {
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "../Public/php/recebe_filmes.php",
+        data: {
+            titulo: "Velozes e Furiosos",
+        },
+        success: function(retorno) {
+           //mostrarModal(JSON.stringify(retorno));
+        }
+    })
+}
+
 function RecebeFilmesServidor() {
     $.ajax({
         type: "POST",
@@ -60,10 +72,8 @@ function mostrarFilmes(param) {
     var auxiliar = JSON.parse(param);
 
     var array = auxiliar.map(function (obj) {  return [obj.ano, obj.duracao, obj.genero, obj.id, obj.imagem, obj.relevancia, obj.sinopse, obj.titulo, obj.trailer]})
-    
 
     $(".produtos").html("")
-
     for (var i = 0; i < array.length; i++) {
         var conteudo = ""
         
@@ -71,15 +81,54 @@ function mostrarFilmes(param) {
         conteudo +=     '<img src="../Public/img/' + array[i][4] + '" class="card-img-top" alt="...">'
         conteudo +=     '<div class="card-body">'
         conteudo +=         '<h5 class="card-title">' + array[i][7] + '</h5>'
-        conteudo +=         '<p class="card-text">' + array[i][2] + '</p>'
-        conteudo +=         '<b class="card-text">' + array[i][0] + '</b>'
+        conteudo +=         '<p class="card-text"> Gênero: ' + array[i][2] + '</p>'
+        conteudo +=         '<p class="card-text"> Ano: ' + array[i][0] + '</p>'
+        conteudo +=         '<br>'
+        conteudo +=         '<button type="button" class="btn btn-primary" id="btnMaisInfos" data-toggle="modal" id_filme="' + i + '" data-target="#exampleModalCenter">Mais Informações</button>'
         conteudo +=     '</div>'
         conteudo += '</div>'
 
-        
-        $(".produtos").append(conteudo)
+         $(".produtos").append(conteudo)
     }
+
+    $(".btn-primary").click(function() {
+        var id = $(this).attr("id_filme")
+        mostrarModal(param, id)      
+    })
+
 }
 
 
+function mostrarModal(param, id) {
+    console.log(id)
+    var auxiliar = JSON.parse(param);
 
+    var array = auxiliar.map(function (obj) {  return [obj.ano, obj.duracao, obj.genero, obj.id, obj.imagem, obj.relevancia, obj.sinopse, obj.titulo, obj.trailer]})
+
+    $(".modal-header").html("")
+    for (var i = 0; i < 1; i++) {
+        var conteudo = ""
+
+        conteudo += '<h5 class="modal-title" id="exampleModalLabel">' + array[id][7] + '</h5>'
+        conteudo += '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'
+        conteudo +=     '<span aria-hidden="true">&times;</span></button>'
+        conteudo += '</button>'
+    
+        $(".modal-header").append(conteudo)
+    }
+
+    $(".modal-body").html("")
+    for (var i = 0; i < 1; i++) {
+        var conteudo = ""
+
+        conteudo +=     '<div class="conteudo-modal">'
+        conteudo +=         '<p> Duração: ' + array[id][1] + '<p>'
+        conteudo +=         '<p> Relevância : ' + array[id][5] + '</p>'
+        conteudo +=         '<p> Sinopse: ' + array[id][6] + '</p>'
+        conteudo +=         '<br>'
+        conteudo +=         '<button type="button" class="btn btn-primary">Assistir Filme</button>'
+        conteudo +=     '</div>'
+
+        $(".modal-body").append(conteudo)
+    }
+}
